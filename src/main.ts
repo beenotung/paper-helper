b7bf5f0579e22d940631019168145d11f30b0459
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-07-08 11:47:47 +0800

    added topic sentense highlight

--- src/main.ts
+++ src/main.ts
@@ -1,131 +1,101 @@
+import construct = Reflect.construct;
 /**
- * Created by beenotung on 7/6/16.
+ * Created by beenotung on 8/6/16.
  */
-console.log('ts main:start');
-// Object.keys(window).map(x=>'key : ' + x).forEach(console.log);
-console.log('ts main:end');
+// printline("start");
 
-function delay(time:number) {
-  return new Promise((resolve, reject)=> {
-    // console.warn('before wait', time);
-    let t = time * Math.random();
-    setTimeout(()=> {
-      // console.log('after wait', time);
-      resolve(t);
-    }, t)
-  });
+function create_and_add_element(tagname:string, target = document.body):HTMLElement {
+  let e = document.createElement(tagname);
+  target.appendChild(e);
+  return e;
 }
 
-declare type Response={
-  ok:boolean;
-  status:number;
-  statusText:string;
-  type:string;
-  url:string;
-  headers:any;
-  body:any;
-  arrayBuffer():any;
-  text():Promise<string>;
-  json():any;
-  blob():any;
+function add_text_field():HTMLInputElement {
+  let e = create_and_add_element('textarea');
+  // e.setAttribute('type', 'textarea');
+  e.style.width = '100%';
+  e.style.height = '10vh';
+  return <HTMLInputElement>e;
+}
+
+let input = add_text_field();
+let output = create_and_add_element('div');
+
+let blacklist = [
+  'This content downloaded from'
+  , 'All use subject to http://about'
+];
+
+function process(input:string):string {
+  // printline('processing', input);
+  let paragraphs = input.split('\n');
+  let topic_sentenses = paragraphs
+    .map(paragraph=> {
+        let sentenses = paragraph.split('.')
+          .filter(x=>!blacklist.some(b=>x.indexOf(b) != -1));
+        return sentenses[0];
+      }
+    );
+  return topic_sentenses.join('\n\n');
+}
+
+function lastChar(s:string) {
+  return s[s.length - 1]
+}
+
+const throw_error = (cons, msg)=> {
+  throw new cons(msg)
 };
 
-declare function fetch(url:string, option:any):Promise<Response>;
+const tail = xs=>xs.length == 0 ? [] : xs.slice(1);
+const head = xs=>xs.length == 0 ? throw_error(ReferenceError, 'array is empty') : xs[0];
 
-type NOOP=()=> void;
+function update() {
+  localStorage.setItem('input', input.value);
+  let lines = input.value.split('\n');
+  let lastline = '  ';
+  let paras = [];
+  lines
+    .filter(line=>!blacklist.some(x=>line.indexOf(x) != -1))
+    .forEach(line=> {
+      // console.debug(line);
+      lastline += ' ' + line;
+      if (lastChar(line) == '.') {
+        paras.push(lastline);
+        lastline = '  ';
+      }
+    });
+  output.innerHTML = '';
 
-module require {
-  type PendingCallback=[NOOP,NOOP];
-  const pending = new Map<string,PendingCallback[]>();
-  const cached = new Set<string>();
 
-  /**@return file extension*/
-  async function checkSource(url:string, cors:boolean):Promise<string> {
-    if (url.indexOf('.') == -1)
-      throw new URIError('no sub-filename detected');
-    let option = {
-      mode: cors ? 'cors' : 'no-cors'
-      , cache: 'force-cache'
+  paras.forEach(para=> {
+    let p = create_and_add_element('p');
+    const add = (string, color = 'black')=> {
+      let e = create_and_add_element('span', p);
+      e.innerText = string;
+      e.style.color = color;
+      e.style.fontFamily='sans-serif'
     };
-    let response = await fetch(url, option);
-    let text = await response.text();
-    if (text.length == 0) {
-      throw new TypeError('empty file')
-    } else {
-      return url.split('.').pop();
-    }
-  }
-
-  async function injectSource(url:string, cors:boolean) {
-    let filetype = await checkSource(url, cors);
-    return new Promise((resolve, reject)=> {
-      switch (filetype) {
-        case 'js':
-          let script = document.createElement('script');
-          script.onload = resolve;
-          script.onerror = reject;
-          script.async = true;
-          script.src = url;
-          document.head.appendChild(script);
-          break;
-        case 'css':
-          let link = document.createElement('link');
-          link.rel = 'stylesheet';
-          link.href = url;
-          document.head.appendChild(link);
-          break;
-        default:
-          throw new TypeError('invalid file type :' + filetype);
-      }
-    });
-  }
-
-  export function load(url:string, cors = false) {
-    return new Promise((resolve:NOOP, reject:NOOP)=> {
-      if (cached.has(url)) {
-        resolve()
-      } else {
-        if (pending.has(url)) {
-          pending.get(url).push([resolve, reject])
-        } else {
-          let xss = [<PendingCallback>[resolve, reject]];
-          pending.set(url, xss);
-          injectSource(url, cors)
-            .then(()=> {
-              cached.add(url);
-              xss.forEach(xs=>xs[0]());
-              pending.delete(url);
-            })
-            .catch(()=> {
-              xss.forEach(xs=>xs[1]());
-              pending.delete(url);
-            });
-        }
-      }
-    });
-  }
-}
-declare const $:any;
-async function printline(...xs:any[]) {
-  await require.load('lib/jquery/dist/jquery.js');
-  console.log('$', $);
-  $(window).ready(()=> {
-    let e = document.createElement('p');
-    e.innerText = xs.join();
-    document.body.appendChild(e);
+    let sentenses = para.split('.');
+    add(head(sentenses), 'red');
+    tail(sentenses).forEach(sentense=>add(sentense));
   });
 }
 
-async function run(time:number) {
-  printline('before run', time);
-  let a = await
-    delay(time);
-  printline('after run', time, a);
-}
+input.onchange = update;
+input.onkeydown = update;
 
-let tasks = Array.from(new Set([1, 2, 3, 4, 4, 2, 1]))
-  .map(x=>x * 1000)
-  .forEach(x=>run(x));
+input.value = localStorage.getItem('input');
+update();
 
-document.head.appendChild(document.createElement('title')).textContent = 'new title';
-// require.load('bundle.css');
+// printline("end");
+
+
+
+
+
+
+
+
+
+
---
+++ src/utils.ts
@@ -0,0 +1,115 @@
+/**
+ * Created by beenotung on 8/6/16.
+ */
+
+function delay(time:number) {
+  return new Promise((resolve, reject)=> {
+    // console.warn('before wait', time);
+    let t = time * Math.random();
+    setTimeout(()=> {
+      // console.log('after wait', time);
+      resolve(t);
+    }, t)
+  });
+}
+
+declare type Response={
+  ok:boolean;
+  status:number;
+  statusText:string;
+  type:string;
+  url:string;
+  headers:any;
+  body:any;
+  arrayBuffer():any;
+  text():Promise<string>;
+  json():any;
+  blob():any;
+};
+
+declare function fetch(url:string, option:any):Promise<Response>;
+
+type NOOP=()=> void;
+
+module require {
+  type PendingCallback=[NOOP,NOOP];
+  const pending = new Map<string,PendingCallback[]>();
+  const cached = new Set<string>();
+
+  /**@return file extension*/
+  async function checkSource(url:string, cors:boolean):Promise<string> {
+    if (url.indexOf('.') == -1)
+      throw new URIError('no sub-filename detected');
+    let option = {
+      mode: cors ? 'cors' : 'no-cors'
+      , cache: 'force-cache'
+    };
+    let response = await fetch(url, option);
+    let text = await response.text();
+    if (text.length == 0) {
+      throw new TypeError('empty file')
+    } else {
+      return url.split('.').pop();
+    }
+  }
+
+  async function injectSource(url:string, cors:boolean) {
+    let filetype = await checkSource(url, cors);
+    return new Promise((resolve, reject)=> {
+      switch (filetype) {
+        case 'js':
+          let script = document.createElement('script');
+          script.onload = resolve;
+          script.onerror = reject;
+          script.async = true;
+          script.src = url;
+          document.head.appendChild(script);
+          break;
+        case 'css':
+          let link = document.createElement('link');
+          link.rel = 'stylesheet';
+          link.href = url;
+          document.head.appendChild(link);
+          break;
+        default:
+          throw new TypeError('invalid file type :' + filetype);
+      }
+    });
+  }
+
+  export function load(url:string, cors = false) {
+    return new Promise((resolve:NOOP, reject:NOOP)=> {
+      if (cached.has(url)) {
+        resolve()
+      } else {
+        if (pending.has(url)) {
+          pending.get(url).push([resolve, reject])
+        } else {
+          let xss = [<PendingCallback>[resolve, reject]];
+          pending.set(url, xss);
+          injectSource(url, cors)
+            .then(()=> {
+              cached.add(url);
+              xss.forEach(xs=>xs[0]());
+              pending.delete(url);
+            })
+            .catch(()=> {
+              xss.forEach(xs=>xs[1]());
+              pending.delete(url);
+            });
+        }
+      }
+    });
+  }
+}
+declare const $:any;
+async function printline(...xs:any[]) {
+  console.log(xs);
+  let e = document.createElement('p');
+  e.innerText = xs.join();
+  document.body.appendChild(e);
+}
+
+async function run(time:number) {
+}
+
